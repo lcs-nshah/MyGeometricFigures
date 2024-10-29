efe8cdb0a55048c46e82da3ddf5fa10a871789cb
Neil Shah <nshah@lcs.on.ca>
2024-10-29 12:21:55 -0400

    Initial Commit

---
+++ MyGeometricFigures/MyGeometricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGeometricFiguresApp.swift
+//  MyGeometricFigures
+//
+//  Created by Neil Shah on 2024-10-29.
+//
+
+import SwiftUI
+
+@main
+struct MyGeometricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
